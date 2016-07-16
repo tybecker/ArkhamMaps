8127a7d2367b2b12e2cf8db9ec9192c7917fdf5e
Benjamin Tyler Becker <tybecker@shaw.ca>
2016-07-16 14:04:18 -0700

    I added and finished map generator 5, with the Miskatonic river, and a bridge or a few over it.  That one took a while to get right.  Anyhow this is probably my last commit for a while.

--- src/maps/main/ArkhamMap.java
+++ src/maps/main/ArkhamMap.java
@@ -48,7 +48,10 @@ public class ArkhamMap {
 	}
 	
 	public int getTile(int x, int y){
-		return mapGrid[x][y];
+		//if(x > 0 && x < WIDTH && y > 0 && y < HEIGHT){
+			return mapGrid[x][y];
+		//}
+		//return 16;
 	}
 	
 	public void setTile(int x, int y, int n){
@@ -197,6 +200,201 @@ public class ArkhamMap {
 		return allowedTiles;
 	}
 	
+	public boolean[] getAllowedTilesNoWater(int x, int y){
+		boolean[] allowedTiles = new boolean[17];
+		
+		for(int i = 0; i < allowedTiles.length; i++){
+			allowedTiles[i] = true;
+		}
+		
+		if(y > 0){
+			if(mapGrid[x][y - 1] == 0){
+				
+			}else if(mapGrid[x][y - 1] == 1 || mapGrid[x][y - 1] == 2 || mapGrid[x][y - 1] == 3 || mapGrid[x][y - 1] == 5 || mapGrid[x][y - 1] == 6 || mapGrid[x][y - 1] == 9 || mapGrid[x][y - 1] == 10 || mapGrid[x][y - 1] == 14){
+				allowedTiles[2] = false;
+				allowedTiles[7] = false;
+				allowedTiles[9] = false;
+				allowedTiles[10] = false;
+				allowedTiles[13] = false;
+				allowedTiles[14] = false;
+				allowedTiles[15] = false;
+				allowedTiles[16] = false;
+			}else{
+				allowedTiles[1] = false;
+				allowedTiles[3] = false;
+				allowedTiles[4] = false;
+				allowedTiles[5] = false;
+				allowedTiles[6] = false;
+				allowedTiles[8] = false;
+				allowedTiles[11] = false;
+				allowedTiles[12] = false;
+			}
+		}else{
+			allowedTiles[1] = false;
+			allowedTiles[3] = false;
+			allowedTiles[4] = false;
+			allowedTiles[5] = false;
+			allowedTiles[6] = false;
+			allowedTiles[8] = false;
+			allowedTiles[11] = false;
+			allowedTiles[12] = false;
+		}
+		
+		if(x < WIDTH - 1){
+			if(mapGrid[x + 1][y] == 0){
+				
+			}else if(mapGrid[x + 1][y] == 1 || mapGrid[x + 1][y] == 2 || mapGrid[x + 1][y] == 3 || mapGrid[x + 1][y] == 4 || mapGrid[x + 1][y] == 7 || mapGrid[x + 1][y] == 10 || mapGrid[x + 1][y] == 11 || mapGrid[x + 1][y] == 15){
+				allowedTiles[3] = false;
+				allowedTiles[6] = false;
+				allowedTiles[10] = false;
+				allowedTiles[11] = false;
+				allowedTiles[12] = false;
+				allowedTiles[14] = false;
+				allowedTiles[15] = false;
+				allowedTiles[16] = false;
+			}else{
+				allowedTiles[1] = false;
+				allowedTiles[2] = false;
+				allowedTiles[4] = false;
+				allowedTiles[5] = false;
+				allowedTiles[7] = false;
+				allowedTiles[8] = false;
+				allowedTiles[9] = false;
+				allowedTiles[13] = false;
+			}
+		}else{
+			allowedTiles[1] = false;
+			allowedTiles[2] = false;
+			allowedTiles[4] = false;
+			allowedTiles[5] = false;
+			allowedTiles[7] = false;
+			allowedTiles[8] = false;
+			allowedTiles[9] = false;
+			allowedTiles[13] = false;
+		}
+		
+		if(y < HEIGHT - 1){
+			if(mapGrid[x][y + 1] == 0){
+				
+			}else if(mapGrid[x][y + 1] == 1 || mapGrid[x][y + 1] == 3 || mapGrid[x][y + 1] == 4 || mapGrid[x][y + 1] == 5 || mapGrid[x][y + 1] == 6 || mapGrid[x][y + 1] == 8 || mapGrid[x][y + 1] == 11 || mapGrid[x][y + 1] == 12){
+				allowedTiles[4] = false;
+				allowedTiles[7] = false;
+				allowedTiles[8] = false;
+				allowedTiles[11] = false;
+				allowedTiles[12] = false;
+				allowedTiles[13] = false;
+				allowedTiles[15] = false;
+				allowedTiles[16] = false;
+			}else{
+				allowedTiles[1] = false;
+				allowedTiles[2] = false;
+				allowedTiles[3] = false;
+				allowedTiles[5] = false;
+				allowedTiles[6] = false;
+				allowedTiles[9] = false;
+				allowedTiles[10] = false;
+				allowedTiles[14] = false;
+			}
+		}else{
+			allowedTiles[1] = false;
+			allowedTiles[2] = false;
+			allowedTiles[3] = false;
+			allowedTiles[5] = false;
+			allowedTiles[6] = false;
+			allowedTiles[9] = false;
+			allowedTiles[10] = false;
+			allowedTiles[14] = false;
+		}
+		
+		if(x > 0){
+			if(mapGrid[x - 1][y] == 0){
+				
+			}else if(mapGrid[x - 1][y] == 1 || mapGrid[x - 1][y] == 2 || mapGrid[x - 1][y] == 4 || mapGrid[x - 1][y] == 5 || mapGrid[x - 1][y] == 7 || mapGrid[x - 1][y] == 8 || mapGrid[x - 1][y] == 9 || mapGrid[x - 1][y] == 13){
+				allowedTiles[5] = false;
+				allowedTiles[6] = false;
+				allowedTiles[8] = false;
+				allowedTiles[9] = false;
+				allowedTiles[12] = false;
+				allowedTiles[13] = false;
+				allowedTiles[14] = false;
+				allowedTiles[16] = false;
+			}else{
+				allowedTiles[1] = false;
+				allowedTiles[2] = false;
+				allowedTiles[3] = false;
+				allowedTiles[4] = false;
+				allowedTiles[7] = false;
+				allowedTiles[10] = false;
+				allowedTiles[11] = false;
+				allowedTiles[15] = false;
+			}
+		}else{
+			allowedTiles[1] = false;
+			allowedTiles[2] = false;
+			allowedTiles[3] = false;
+			allowedTiles[4] = false;
+			allowedTiles[7] = false;
+			allowedTiles[10] = false;
+			allowedTiles[11] = false;
+			allowedTiles[15] = false;
+		}
+		
+		//Now check for rivers.
+		if(x > 0){
+			if(riverGrid[x - 1][y] == 1 && !(mapGrid[x - 1][y] == 1 || mapGrid[x - 1][y] == 2 || mapGrid[x - 1][y] == 4 || mapGrid[x - 1][y] == 5 || mapGrid[x - 1][y] == 7 || mapGrid[x - 1][y] == 8 || mapGrid[x - 1][y] == 9 || mapGrid[x - 1][y] == 13)){
+				allowedTiles[1] = false;
+				allowedTiles[2] = false;
+				allowedTiles[3] = false;
+				allowedTiles[4] = false;
+				allowedTiles[7] = false;
+				allowedTiles[10] = false;
+				allowedTiles[11] = false;
+				allowedTiles[15] = false;
+			}
+		}
+		
+		if(x < WIDTH - 1){
+			if(riverGrid[x + 1][y] == 1 && !(mapGrid[x + 1][y] == 1 || mapGrid[x + 1][y] == 2 || mapGrid[x + 1][y] == 3 || mapGrid[x + 1][y] == 4 || mapGrid[x + 1][y] == 7 || mapGrid[x + 1][y] == 10 || mapGrid[x + 1][y] == 11 || mapGrid[x + 1][y] == 15)){
+				allowedTiles[1] = false;
+				allowedTiles[2] = false;
+				allowedTiles[4] = false;
+				allowedTiles[5] = false;
+				allowedTiles[7] = false;
+				allowedTiles[8] = false;
+				allowedTiles[9] = false;
+				allowedTiles[13] = false;
+			}
+		}
+		
+		if(y > 0){
+			if(riverGrid[x][y - 1] == 1 && !(mapGrid[x][y - 1] == 1 || mapGrid[x][y - 1] == 2 || mapGrid[x][y - 1] == 3 || mapGrid[x][y - 1] == 5 || mapGrid[x][y - 1] == 6 || mapGrid[x][y - 1] == 9 || mapGrid[x][y - 1] == 10 || mapGrid[x][y - 1] == 14)){
+				allowedTiles[1] = false;
+				allowedTiles[3] = false;
+				allowedTiles[4] = false;
+				allowedTiles[5] = false;
+				allowedTiles[6] = false;
+				allowedTiles[8] = false;
+				allowedTiles[11] = false;
+				allowedTiles[12] = false;
+			}
+		}
+		
+		if(y < HEIGHT - 1){
+			if(riverGrid[x][y + 1] == 1 && !(mapGrid[x][y + 1] == 1 || mapGrid[x][y + 1] == 3 || mapGrid[x][y + 1] == 4 || mapGrid[x][y + 1] == 5 || mapGrid[x][y + 1] == 6 || mapGrid[x][y + 1] == 8 || mapGrid[x][y + 1] == 11 || mapGrid[x][y + 1] == 12)){
+				allowedTiles[1] = false;
+				allowedTiles[2] = false;
+				allowedTiles[3] = false;
+				allowedTiles[5] = false;
+				allowedTiles[6] = false;
+				allowedTiles[9] = false;
+				allowedTiles[10] = false;
+				allowedTiles[14] = false;
+			}
+		}
+		
+		return allowedTiles;
+	}
+	
 	public boolean hasAdjacentConnectedIntersection(int x, int y){
 		boolean adjacent = false;
 		
@@ -332,7 +530,10 @@ public class ArkhamMap {
 	}
 	
 	public int getRiver(int x, int y){
-		return riverGrid[x][y];
+		if(x > 0 && x < WIDTH && y > 0 && y < HEIGHT){
+			return riverGrid[x][y];
+		}
+		return 1;
 	}
 	//Planned methods:
 	//public boolean hasAdjacentCorner(int x, int y)
--- src/maps/main/MapGenerator5.java
+++ src/maps/main/MapGenerator5.java
@@ -52,8 +52,8 @@ public class MapGenerator5 extends MapGenerator{
 			
 			yHeight = yHeight + change;
 			
-			if(yHeight < 2){
-				yHeight = 2;
+			if(yHeight < 3){
+				yHeight = 3;
 			}
 			
 			if(yHeight > m.HEIGHT - 4){
@@ -65,8 +65,157 @@ public class MapGenerator5 extends MapGenerator{
 		}
 		
 		//Generate Trunk roads
-				
+		for(int i = 0; i < m.HEIGHT; i++){
+			m.setTile(14, i, 6);
+		}
+		
+		int mainEastRoad = 0;
+		if(m.getRiver(14, 9) == 0 && m.getRiver(15, 9) == 0){
+			m.setTile(14, 9, 5);
+			mainEastRoad = 9;
+		}else{
+			m.setTile(14, 6, 5);
+			mainEastRoad = 6;
+		}
+		
+		for(int i = 15; i < m.WIDTH; i++){
+			if(m.getRiver(i + 1, mainEastRoad) == 0){
+				m.setTile(i, mainEastRoad, 7);
+			}else if(m.getRiver(i, mainEastRoad + 1) == 1){
+				m.setTile(i, mainEastRoad, 11);
+				mainEastRoad = mainEastRoad - 1;
+				m.setTile(i, mainEastRoad, 9);
+			}else if(m.getRiver(i, mainEastRoad - 1) == 1){
+				m.setTile(i, mainEastRoad, 10);
+				mainEastRoad = mainEastRoad + 1;
+				m.setTile(i, mainEastRoad, 8);
+			}else{
+				m.setTile(i, mainEastRoad, 7);
+			}
+		}
+		
+		
+		for(int i = 0; i < m.WIDTH; i++){
+			for(int j = 0; j < m.HEIGHT; j++){
+				if(m.hasAdjacentConnectedIntersection(i, j)){
+					
+				}else{
+					if(m.getTile(i, j) == 6){
+						double random = Math.random();
+						if(m.getRiver(i + 1, j) == 1 && m.getRiver(i, j) == 0){
+							if(random < 0.3){
+								m.setTile(i, j, 3);
+							}
+						}else if(m.getRiver(i - 1, j) == 1 && m.getRiver(i, j) == 0){
+							if(random < 0.3){
+								m.setTile(i, j, 5);
+							}
+						}else if (m.getRiver(i, j) == 0){
+							if(random < 0.1){
+								m.setTile(i, j, 1);
+							}else if(random < 0.2){
+								m.setTile(i, j, 3);
+							}else if(random < 0.3){
+								m.setTile(i, j, 5);
+							}
+						}else{
+							
+						}
+					}else if(m.getTile(i, j) == 7){
+						double random = Math.random();
+						if(random < 0.1){
+							m.setTile(i, j, 1);
+						}else if(random < 0.2){
+							m.setTile(i, j, 2);
+						}else if(random < 0.3){
+							m.setTile(i, j, 4);
+						}
+					}else if(m.getTile(i, j) == 8){
+						double random = Math.random();
+						if(random < 0.1){
+							m.setTile(i, j, 5);
+						}
+					}else if(m.getTile(i, j) == 9){
+						double random = Math.random();
+						if(random < 0.1){
+							m.setTile(i, j, 5);
+						}
+					}else if(m.getTile(i, j) == 10){
+						double random = Math.random();
+						if(random < 0.1){
+							m.setTile(i, j, 3);
+						}
+					}else if(m.getTile(i, j) == 11){
+						double random = Math.random();
+						if(random < 0.1){
+							m.setTile(i, j, 3);
+						}
+					}else{
+						
+					}
+				}
+			}
+		}
+		
 		//Generate offshoots
+		for(int i = 0; i < m.WIDTH; i++){
+			for(int j = 0; j < m.HEIGHT; j++){
+				if(m.hasAdjacentConnected(i, j) && m.getRiver(i, j) == 1){
+					m.setTile(i, j, 6);
+				}
+			}
+		}
+		
+		for(int i = 0; i < m.WIDTH; i++){
+			for(int j = 0; j < m.HEIGHT; j++){
+				if(m.hasAdjacentConnected(i, j) && m.getRiver(i, j) == 1){
+					m.setTile(i, j, 6);
+				}
+			}
+		}
+		
+		//Fill town.
+		while(m.hasOpenConnections()){
+			for(int i = 0; i < m.WIDTH; i++){
+				for(int j = 0; j < m.HEIGHT; j++){
+					if(m.hasAdjacentConnected(i, j) && m.getTile(i, j) == 0){
+						boolean[] allowed = m.getAllowedTilesNoWater(i, j);
+						int[] weights = new int[allowed.length];
+						for(int k = 0; k < allowed.length; k++){
+							if(allowed[k]){
+								weights[k] = 1;
+							}else{
+								weights[k] = 0;
+							}
+						}
+						
+						int sum = 0;
+						
+						for(int k = 0; k < weights.length; k++){
+							sum = sum + weights[k];
+						}
+						
+						double random = Math.random();
+						
+						random = random * sum;
+						
+						boolean notChosen = true;
+						for(int k = 0; k < weights.length; k++){
+							random = random - weights[k];
+							if(random < 0 && notChosen == true){
+								m.setTile(i, j, k);
+								notChosen = false;
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		if(m.hasOpenConnections()){
+			System.out.println("This is a problem map!");
+		}
+		
 		m.fillZeroes();
 	}
 
